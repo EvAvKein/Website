70a1ccd641f06a73ad67770a9dba69013a8b3b23
Eve Aviv Keinan <evavkein@gmail.com>
2022-08-25 20:10:32 +0300

    refactor (core css): replace ':global' wrappers with 'global' style block attribute
    i originally looked this up before saw people saying that the only way to declare global style in svelte is with the wrappers, but i've just found out that's not the case (anymore?)...

--- src/routes/core.svelte
+++ src/routes/core.svelte
@@ -30,8 +30,8 @@
   };
 </script>
 
-<style>
-  :global(:root) { /* filters created via https://codepen.io/sosuke/pen/Pjoqqp */
+<style global>
+  :root { /* filters created via https://codepen.io/sosuke/pen/Pjoqqp */
     --textColor: #e9e9e9;
     --filterToTextColor: brightness(0) saturate(100%) invert(100%) sepia(1%) saturate(244%) hue-rotate(180deg) brightness(115%) contrast(83%);
     --textSubColor: #bebebe;
@@ -46,7 +46,7 @@
     --filterToHighlightSubColor: brightness(0) saturate(100%) invert(62%) sepia(76%) saturate(412%) hue-rotate(9deg) brightness(102%) contrast(84%);
   }
 
-  :global(body) {
+  body {
     margin: 0;
     min-height: 100%;
     box-sizing: border-box;
@@ -58,49 +58,49 @@
     font-size: clamp(0.9em, 2.5vw, 1.1em);
   }
 
-  :global(img) {
+  img {
     pointer-events: none;
     user-select: none;
   }
 
-  :global(h1), :global(h2), :global(h3), :global(h4), :global(h5), :global(h6), :global(p) {margin: 0}
+  h1, h2, h3, h4, h5, h6, p {margin: 0}
 
-  :global(a) {
+  a {
     color: var(--textColor);
     text-decoration: none;
   }
 
-  :global(button) {font-size: inherit}
-  :global(button), :global(a), :global(summary) {
+  button {font-size: inherit}
+  button, a, summary {
     cursor: pointer;
     border: none;
     outline: none;
   }
 
-  :global(::-webkit-scrollbar) {
+  ::-webkit-scrollbar {
     width: 1rem;
     height: 1rem;
   }
-  :global(::-webkit-scrollbar-track) {
+  ::-webkit-scrollbar-track {
     background-color: var(--backgroundColor);
     margin: 0.2rem;
   }
-  :global(::-webkit-scrollbar-corner) {
+  ::-webkit-scrollbar-corner {
     background-color: var(--backgroundColor)
   }
-  :global(::-webkit-scrollbar-thumb) {
+  ::-webkit-scrollbar-thumb {
     background-color: var(--textColor);
     border: 0.25rem solid var(--backgroundColor);
     border-radius: 3rem;
   }
-  :global(::-webkit-scrollbar-thumb:hover) {
+  ::-webkit-scrollbar-thumb:hover {
     background-color: var(--textSubColor);
   }
 
   /*~~~~~~~~~~~~~~~~~~~~~\
     CORE STYLING CLASSES
   \~~~~~~~~~~~~~~~~~~~~~*/
-  :global(.core_backgroundButton) {
+  .core_backgroundButton {
     color: var(--backgroundColor);
     background-color: var(--textColor);
     border-radius: 0.2em;
@@ -108,33 +108,33 @@
     padding: 0.25em 0.5em;
     border-radius: 0.5em;
   }
-  :global(.core_backgroundButton:focus), :global(.core_backgroundButton:hover) {
+  .core_backgroundButton:focus, .core_backgroundButton:hover {
     background-color: var(--highlightSubColor);
   }
-  :global(.core_backgroundButton:active) {
+  .core_backgroundButton:active {
     background-color: var(--highlightColor);
   }
 
-  :global(.core_contentButton) {
+  .core_contentButton {
     color: inherit;
     background-color: transparent;
     padding: 0;
   }
-  :global(.core_contentButton img, .core_contentButton svg) {
+  .core_contentButton img, .core_contentButton svg {
     height: inherit;
     filter: var(--filterToTextColor);
   }
-  :global(.core_contentButton:focus), :global(.core_contentButton:hover) {
+  .core_contentButton:focus, .core_contentButton:hover {
     color: var(--highlightSubColor);
   }
-  :global(.core_contentButton:active) {
+  .core_contentButton:active {
     color: var(--highlightColor);
   }
-  :global(.core_contentButton:focus img, .core_contentButton:focus svg),
-  :global(.core_contentButton:hover img, .core_contentButton:hover svg) {
+  .core_contentButton:focus img, .core_contentButton:focus svg,
+  .core_contentButton:hover img, .core_contentButton:hover svg {
     filter: var(--filterToHighlightSubColor);
   }
-  :global(.core_contentButton:active img, .core_contentButton:active svg) {
+  .core_contentButton:active img, .core_contentButton:active svg {
     filter: var(--filterToHighlightColor);
   }
 </style>
