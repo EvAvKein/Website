0bba54e465479cff47421c96395f98768be35a91
Eve_Aviv <evavkein@gmail.com>
2022-09-26 13:12:09 +0300

    refactor (about): adjustments in "what" & "who" sections

--- src/routes/_QnAs/what.svelte
+++ src/routes/_QnAs/what.svelte
@@ -2,9 +2,9 @@
   <h2>What?</h2>
   <h3>(Activities)</h3>
   <ul>
-    <li>Fullstack programming (but predominantly front-end)</li>
-    <li>Video-games of a rotating mix of genres: RPGs, team-based/co-op shooters, platformers, MMOs (very rarely), and a wide variety of indie</li>
-    <li>...And a wide variety of video/audio media (e.g news, explainers, fandoms, modified 2E DnD)</li>
+    <li>Web programming (fullstack, but primarily frontend)</li>
+    <li>Video-games (e.g RPGs, team-based/co-op shooters, platformers, adventure/narrative-centric, MMOs (very rarely), misc indie)</li>
+    <li>Online media (e.g news, explainers, fandoms)</li>
   </ul>
 </section>
 
--- src/routes/_QnAs/who.svelte
+++ src/routes/_QnAs/who.svelte
@@ -2,7 +2,7 @@
   <h2>Who?</h2>
   <h3>(Demographics)</h3>
   <ul>
-    <li>Progressive leftist (as derived from Utilitarianism)</li>
+    <li>Progressive leftie (deriving from Utilitarianism)</li>
     <li>Neuroatypical (HFASD & GAD & ADD)</li>
     <li>Cis/agender guy (he/they)</li>
     <li>Senior Gen-Z</li>
