760236fc377973bef505580eb7c05d3b52839138
Eve_Aviv <evavkein@gmail.com>
2022-09-30 17:07:10 +0300

    refactor (about page): layout improvements
    not final, but better than it the previous blank slate (which i was going to do another direction with, but changed my mind for now)

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,49 +3,53 @@
   headDescription="The home page of Eve Aviv Keinan's personal and portfolio website"
 >
   <section id="about">
-    <section>
-      <h2>Who?</h2>
-      <h3>(Demographics)</h3>
-      <ul>
-        <li>Progressive leftie <span>(deriving from Utilitarianism)</span></li>
-        <li>Neuroatypical <span>(HFASD & GAD & ADD)</span></li>
-        <li>Cis/agender guy <span>(he/they)</span></li>
-        <li>Senior Gen-Z</li>
-      </ul>
-    </section>
+    <h1>About Me</h1>
+    <hr/>
 
-    <section>
-      <h2>What?</h2>
-      <h3>(Activities)</h3>
-      <ul>
-        <li>Web programming <span>(fullstack, but primarily frontend)</span></li>
-        <li>Video-games <span>(e.g RPGs, adventure, shooters, misc indie)</span></li>
-        <li>Online media <span>(e.g news, explainers, fandoms)</span></li>
-      </ul>
-    </section>
-
-    <section>
-
-      <h2>How?</h2>
-      <h3>(Values)</h3>
-      <ul>
-        <li>Transparency</li>
-        <li>Introspection</li>
-        <li>Prudence</li>
-        <li>Dicipline</li>
-        <li>Compassion</li>
-      </ul>
-    </section>
-
-    <section>
-      <h2>Where?</h2>
-      <h3>(Residence)</h3>
-      <p>Currently in Israel. Thanks to my French citizenship, I'll immigrate to the EU 
-        <Tooltip>
-          <span slot="subject">(likely Finland/Sweden)</span>
-          <span slot="content">(for interpersonal & politics reasons)</span>
-        </Tooltip>
-        once I'm employed remotely/there</p>
+    <section id="QnA">
+      <section id="who">
+        <h2>Who?</h2>
+        <h3>(Demographics)</h3>
+        <ul>
+          <li>Progressive leftie <span>(deriving from Utilitarianism)</span></li>
+          <li>Neuroatypical <span>(HFASD & GAD & ADD)</span></li>
+          <li>Cis/agender guy <span>(he/they)</span></li>
+          <li>Senior Gen-Z</li>
+        </ul>
+      </section>
+  
+      <section id="how">
+        <h2>How?</h2>
+        <h3>(Values)</h3>
+        <ul>
+          <li>Transparency</li>
+          <li>Introspection</li>
+          <li>Prudence</li>
+          <li>Dicipline</li>
+          <li>Compassion</li>
+        </ul>
+      </section>
+  
+      <section id="what">
+        <h2>What?</h2>
+        <h3>(Activities)</h3>
+        <ul>
+          <li>Web programming <span>(fullstack, but primarily frontend)</span></li>
+          <li>Video-games <span>(e.g RPGs, adventure, shooters, misc indie)</span></li>
+          <li>Online media <span>(e.g news, explainers, fandoms)</span></li>
+        </ul>
+      </section>
+  
+      <section id="where">
+        <h2>Where?</h2>
+        <h3>(Residence)</h3>
+        <p>Currently in Israel. Thanks to my French citizenship, I'll immigrate to the EU 
+          <Tooltip>
+            <span slot="subject">(likely Finland/Sweden)</span>
+            <span slot="content">(for interpersonal & politics reasons)</span>
+          </Tooltip>
+          once I'm employed remotely/there</p>
+      </section>
     </section>
   </section>
 </Core>
@@ -56,20 +60,30 @@
 </script>
 
 <style>
-  #about {
-    font-size: clamp(1.25em, 3vw, 1.5em);
-    max-width: 30em;
+  #QnA {
     padding: 1em 0.5em 0.5em;
+    max-width: 80em;
     margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1em 2em;
+  }
+  @media (min-width: 36em) {
+    #QnA {justify-content: space-around}
   }
 
-  section + section {margin-top: 2em}
-
-  h2 {font-size: 2em}
+  h1 {
+    font-size: 3.5em;
+    text-align: center;
+  }
+  
+  h2 {font-size: 2.25em}
   h3 {
-    font-size: 1em;
+    font-size: 1.5em;
     color: var(--textSubColor);
   }
+
+  li, p {font-size: 1.25em}
 
   li span {
     color: var(--textSubColor);
