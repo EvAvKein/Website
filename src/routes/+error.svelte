93fa02ae1e803a8873c0be0ecdff7ca44f8d8764
Eve_Aviv <evavkein@gmail.com>
2022-11-01 08:33:30 +0200

    feat: different error page text when device is offline

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,16 +3,25 @@
   headDescription="Eve Aviv Keinan's website's error/404 page; including the site's navigation list"
 >
   <main>
-    <h1>404: Page not found!</h1>
-    <hr/>
-    <p>Maybe you'll find your destination in the navigation menu? ¯\_(ツ)_/¯</p>
+    {#if offline}
+      <h1>Error: No connection!</h1>
+      <hr/>
+      <p>Looks like your device is offline, check your connection and refresh once it's back up? ¯\_(ツ)_/¯</p>
+    {:else}
+      <h1>404: Page not found!</h1>
+      <hr/>
+      <p>Maybe you'll find your destination in the navigation menu? ¯\_(ツ)_/¯</p>
+    {/if}
   </main>
 </Core>
 
 <script lang="ts">
   import Core from "./_core.svelte";
-</script>
+  import {onMount} from "svelte";
 
+  let offline:boolean;
+  onMount(() => {offline = !window.navigator.onLine});
+</script>
 
 <style>
   main {
