6ce4912670ff3c9e5f60411dc53a50f7ac9fbb8b
Eve_Aviv <evavkein@gmail.com>
2023-01-13 10:27:18 +0200

    refactor (portfolio): remove unused techsSection wrapper & css declaration

--- src/routes/portfolio/techs/techsSection.svelte
+++ src/routes/portfolio/techs/techsSection.svelte
@@ -1,55 +1,53 @@
-<section>
-  <section id="techs">
-    <section>
-      <h4>
-        <Tooltip
-          text={"Languages"}
-          tooltip={"...and frameworks, libraries, etc"}
-        />
-      </h4>
-      <TechsBox bind:selectedTech
-        techsByNameAndSrc={[
-          ["HTML", "/techs/html.svg"],
-          ["CSS", "/techs/css.svg"],
-          ["JavaScript", "/techs/javascript.svg"],
-          ["TypeScript", "/techs/typescript.svg"],
-          ["Vue", "/techs/vue.svg"],
-          ["Svelte & SvelteKit", "/techs/svelte.svg"],
-          ["MongoDB", "/techs/mongoDb.svg"],
-          ["Express", "/techs/express.svg"],
-          ["Cypress", "/techs/cypress.svg"]
-        ]}
+<section id="techs">
+  <section>
+    <h4>
+      <Tooltip
+        text={"Languages"}
+        tooltip={"...and frameworks, libraries, etc"}
       />
-    </section>
-    <hr>
-    <section>
-      <h4>Tools</h4>
-      <TechsBox bind:selectedTech
-        techsByNameAndSrc={[
-          ["Visual Studio Code", "/techs/vsCode.svg"],
-          ["Chrome DevTools", "/techs/chromeDevtools.svg"],
-          ["GitHub", "/techs/github.svg"],
-          ["Figma", "/techs/figma.svg"],
-          ["Git", "/techs/git.svg"],
-          ["Docker", "/techs/docker.svg"],
-          ["Prettier", "/techs/prettier.svg"],
-          ["Node.js", "/techs/nodeJs.svg"]
-        ]}
-      />
-    </section>
+    </h4>
+    <TechsBox bind:selectedTech
+      techsByNameAndSrc={[
+        ["HTML", "/techs/html.svg"],
+        ["CSS", "/techs/css.svg"],
+        ["JavaScript", "/techs/javascript.svg"],
+        ["TypeScript", "/techs/typescript.svg"],
+        ["Vue", "/techs/vue.svg"],
+        ["Svelte & SvelteKit", "/techs/svelte.svg"],
+        ["MongoDB", "/techs/mongoDb.svg"],
+        ["Express", "/techs/express.svg"],
+        ["Cypress", "/techs/cypress.svg"]
+      ]}
+    />
   </section>
-  {#if selectedTech}
-    <hr transition:slide>
-    {#key selectedTech}
-      <div transition:slide>
-        <p 
-          out:fly={{y: -100, opacity: 0}}
-          in:fly={{y: -100, opacity: 0}}
-        >{selectedTech}</p>
-      </div>
-    {/key}
-  {/if}
+  <hr>
+  <section>
+    <h4>Tools</h4>
+    <TechsBox bind:selectedTech
+      techsByNameAndSrc={[
+        ["Visual Studio Code", "/techs/vsCode.svg"],
+        ["Chrome DevTools", "/techs/chromeDevtools.svg"],
+        ["GitHub", "/techs/github.svg"],
+        ["Figma", "/techs/figma.svg"],
+        ["Git", "/techs/git.svg"],
+        ["Docker", "/techs/docker.svg"],
+        ["Prettier", "/techs/prettier.svg"],
+        ["Node.js", "/techs/nodeJs.svg"]
+      ]}
+    />
+  </section>
 </section>
+{#if selectedTech}
+  <hr transition:slide>
+  {#key selectedTech}
+    <div transition:slide>
+      <p 
+        out:fly={{y: -100, opacity: 0}}
+        in:fly={{y: -100, opacity: 0}}
+      >{selectedTech}</p>
+    </div>
+  {/key}
+{/if}
 
 
 <script lang="ts">
@@ -62,7 +60,6 @@
 
 <style>
   #techs {display: flex}
-  #techs > section {flex-basis: 1}
 
   h4 {
     font-size: 2em;
