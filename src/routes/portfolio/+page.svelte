c0eb7e3fdf6cfd46dbf0ee5038f9cc717a296282
Eve_Aviv <evavkein@gmail.com>
2023-01-16 07:56:39 +0200

    refactor (portfolio): delete dispositions section
    deleted for the following reasons:
    - not a fan of it ended up, i'd have to reformulate it per my usual high and pedantic standards if it wasn't for this commit
    - it's basically an 'about' text in the portfolio, and i already have an 'about' page
    - since writing this section i've added a "values" section to my 'about'/home page, which has a lot of overlapping exposition and should ideally cover everything that matters about this section
    - the introductory paragraph highlights my lack of professional experience (and that paragraph seems like a mandatory explanation for why that text is a the portfolio page)

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,6 +1,6 @@
 <Core
   headTitle="EvAvKein: Portfolio"
-  headDescription="Eve Aviv Keinan's programming portfolio page; including summaries with links to his projects, lists of the techs he has experience with, dispositions relevant to his professional conduct, and information regarding his resumé"
+  headDescription="Eve Aviv Keinan's programming portfolio page; including summaries with links to his projects, lists of the techs he has experience with, and information regarding his resumé"
 >
   <section id="portfolio">
     <h2>Portfolio</h2>
@@ -18,48 +18,6 @@
       <h3>Resumé</h3>
       <p>The summary is that I've done civil service and community management, you're welcome to contact me for the resumé file with all its usual details</p>
     </section>
-
-    <section id="dispositions" class="core_blueprint">
-      <h3>Dispositions</h3>
-        <p>
-          Due to my lack of professional experience in the field, here are (some) reasons why I'm dispositionally fit for software engineering:
-        </p>
-        <ul>
-          <li>
-            Thanks to my neuroatypicality, ongoing introspection, and ethics, I'm particularly analytically-inclined: I have a keen eye for edge-cases and incongruities, and 
-            <Tooltip
-              text={"prefer long-term/systemic solutions"}
-              tooltip={"(or, when there's insufficient time/context, at least the likeliest educated guess)"}
-            />
-            over instantly-gratifying haphazard ones
-          </li>
-          <li>
-            I'm mindful of the well-being of those I affect: Heeding Utilitarian calculus in all domains,
-            <Tooltip
-              text={"without disregarding"}
-              tooltip={"(but outright exonerating as few as viable)"}
-            />
-            human imperfections/conditions
-          </li>
-          <li>
-            I aim to be 
-            <Tooltip
-              text={"as courteous as possible"}
-              tooltip={"(as long as doing so doesn't conflict with Utilitarianism)"}
-            />, 
-            <Tooltip
-              text={"'meet people where they're at'"}
-              tooltip={"(as much as viable, e.g citing Utilitarianism seemed too necessary to sufficiently convey my perspective)"}
-            />,
-            and (at the very least) be a 
-            <Tooltip
-              text={"worthwhile participant"}
-              tooltip={"(compared to the time/attention/resources my presence/conduct necessitate)"}
-            />
-            in the communities/systems I engage with
-          </li>
-        </ul>
-    </section>
   </section>
 </Core>
 
@@ -67,8 +25,6 @@
   import Core from "../_core.svelte";
   import Techs from "./techs/techsSection.svelte";
   import Projects from "./projects/projectsSection.svelte";
-
-  import Tooltip from "../../lib/tooltip.svelte";
 </script>
 
 <style>
@@ -93,6 +49,4 @@
     margin: auto;
     max-width: 45em;
   }
-
-  li + li {margin-top: 0.4em}
 </style>
