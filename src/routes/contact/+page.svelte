f3200f445b56c56bccefbfe2fb9cde38bad573df
Eve_Aviv <evavkein@gmail.com>
2023-04-10 14:03:10 +0300

    refactor (contact page): comprehensive refactor
    would've bothered splitting this into multiple commits, or writing a complete description for this one, if this was a group project... but it's a solo project for my personal website, and i can allow myself to just not bother with that (especially after doing it minutes ago with multiple commits)

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -4,14 +4,13 @@
 >
   <section id="contact">
     <h2>Contact</h2>
-    <form>
-      {#if typeof notifDesirability !== "boolean"}
+    {#if typeof notifDesirability !== "boolean"}  
+      <form>
         <LabelledInput
           id={"contactFormTitle"}
           label={"Title (optional)"}
           bind:value={title}
         />
-        <hr>
         <LabelledInput
           id={"contactFormBody"}
           type={"textarea"}
@@ -19,29 +18,33 @@
           bind:value={body}
           textareaMinLineHeight={10}
         />
-        <hr>
         <LabelledInput
           id={"contactFormEmail"}
           label={"Email"}
           bind:value={email}
         />
-      {/if}
-      <Notification bind:text={notifText} desirablityStyle={notifDesirability} baseDuration={null}/>
-      {#if body && email && typeof notifDesirability !== "boolean"}
-      <div id="submitWrapper"
-        transition:slide
-      >
-        <button
-          transition:fly={{y: -100}}
-          class="core_backgroundButton"
-          type="button"
-          on:click={submitForm}
-        >Submit</button>
-      </div>
-      {/if}
-    </form>
 
-    <span>...or email me at
+        {#if body && email}
+        <div id="submitWrapper"
+          transition:slide
+        >
+          <button
+            transition:fly={{y: -100}}
+              class="core_backgroundButton"
+              type="button"
+              on:click={submitForm}
+          >Send</button>
+        </div>
+        {/if}
+      </form>
+    {/if}
+
+    <Notification bind:text={notifText} desirablityStyle={notifDesirability} baseDuration={null}/>
+
+    <span>
+      {#if notifDesirability !== false}
+        ...or email me at
+      {/if}
       <button
         class="core_contentButton"
         on:click={copyEmail}
@@ -65,33 +68,31 @@
   onMount(() => myEmailAddress = "evavkein@gmail.com"); // adding via JS to mitigate scraping
   let emailCopyNotice = "";
   function copyEmail() {
-    navigator.clipboard.writeText(myEmailAddress)
-      .then(() => {emailCopyNotice = "Address copied to clipboard!"});
+    navigator.clipboard.writeText(myEmailAddress).then(() => {
+      emailCopyNotice = "Address copied to clipboard!"
+    });
   };
 
   let title = "";
   let body = "";
   let email = "";
 
-  let notifText = "" as ""|typeof notifTexts[keyof typeof notifTexts];
-  const notifTexts = {
-    pending: "Processing...",
-    success: "Message sent! I usually reply within a day",
-    failure: "Message submission failed, you'll have to email me instead",
-  } as const;
+  let notifText = "";
   let notifDesirability = undefined as boolean|undefined;
 
   function submitForm() {
-    notifText = notifTexts.pending;
+    notifText = "Processing...";
     notifDesirability = undefined;
 
     apiFetch("POST", "/submitContactForm", {title: title, body: body, email: email})
       .then((response:{outcome:boolean}) => {
-        notifText = response.outcome ? notifTexts.success : notifTexts.failure;
-        notifDesirability = response.outcome;
+        if (response.outcome) throw response;
+
+        notifText = "Message sent! I usually reply within a day";
+        notifDesirability = true;
       })
       .catch(() => {
-        notifText = notifTexts.failure;
+        notifText = "Message submission failed, you'll have to email me instead";
         notifDesirability = false;
       });
   };
@@ -99,26 +100,24 @@
 
 <style>
   #contact {
-    width: clamp(20em, 85vw, 50em);
+    font-size: 1.5em;
+    width: clamp(12.5em, 85vw, 35em);
     text-align: center;
-    padding: 0 0.75em 0.25em;
+    padding: 0 0.5em 0.2em;
   }
 
   #contact > * {margin-inline: auto}
 
-  h2 {font-size: 4em}
-  form {font-size: 1.5em}
-  hr {
-    margin: 0.2em;
-    border-color: transparent;
+  h2 {font-size: 2.5em}
+  
+  form button {
+    margin-top: 1em;
   }
 
-  form #submitWrapper {padding-top: 1em}
-
   span {
+    font-size: 0.9em;
     display: inline-block;
     margin-top: 1em;
-    font-size: 1.25em;
   }
   address {
     display: inline;
