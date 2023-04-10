a14fb564125606dc707f16167a8a8d960992685a
Eve_Aviv <evavkein@gmail.com>
2023-04-10 13:52:24 +0300

    refactor (evavkein.svelte): remove configurable/explicit layouts
    initially wrote that code to give myself options/versatility, but this simplified code would allow the same options via a limited-width wrapper

--- src/routes/evavkein.svelte
+++ src/routes/evavkein.svelte
@@ -1,43 +1,17 @@
-<h1 bind:this={wrapper}
-  class={layout}
-  aria-label="Eve Aviv Keinan"
->
+<h1 aria-label="Eve Aviv Keinan">
   <span aria-hidden="true" id="eve" >Ev<span class="extra">e</span></span>
   <span aria-hidden="true" id="aviv">Av<span class="extra">iv</span></span>
   <span aria-hidden="true" id="keinan">Kein<span class="extra">an</span></span>
 </h1>
 
-<script lang="ts">
-  import {onMount} from "svelte";
-
-  let wrapper:HTMLElement;
-  let layout:"tight"|"moderate"|"spacious";
-
-  let wrapperObserver:ResizeObserver;
-
-  $: if (wrapper) {
-    wrapperObserver = new ResizeObserver(() => {
-      const computedStyles = window.getComputedStyle(wrapper);
-      const fontSize = Number.parseFloat(computedStyles.fontSize);
-      const width = Number.parseFloat(computedStyles.width);
-      const emWidth = width / fontSize;
-      
-      if (emWidth > 7.9) {layout = "spacious"}
-      else if (emWidth > 4.25) {layout = "moderate"}
-      else {layout = "tight"}
-    });
-
-    wrapperObserver.observe(wrapper);
-  };
-
-  onMount(() => {return () => {wrapperObserver.disconnect()}});
-</script>
-
 <style>
   h1 {
-    font-size: clamp(3.5em, 11vw, 5.5em);
+    font-size: clamp(3.5em, 11vw, 5em);
     color: var(--highlightColor);
     user-select: none;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
     gap: 0 0.3em;
     transition: gap 750ms ease-in-out;
   }
@@ -57,33 +31,5 @@
     opacity: 0;
     transition: max-width 550ms ease-out 100ms,
                 opacity 400ms ease-out;
-  }
-
-  .tight > span {
-    display: block;
-    text-align: center;
-  }
-
-  .moderate {
-    display: grid;
-    grid-template-areas: "eve aviv"
-                         "keinan keinan";
-  }
-  .moderate #eve {
-    grid-area: eve;
-    justify-self: right;
-  }
-  .moderate #aviv {
-    grid-area: aviv;
-    justify-self: left;
-  }
-  .moderate #keinan {
-    grid-area: keinan;
-    justify-self: center;
-  }
-
-  .spacious {
-    display: flex;
-    justify-content: center;
   }
 </style>
